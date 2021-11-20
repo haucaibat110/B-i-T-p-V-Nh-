e60aa2760f18a20484932af36d265211ab2755a9
haucaibat110 <88539718+haucaibat110@users.noreply.github.com>
2021-11-20 15:47:07 +0700

    Update Code Challenge.js

--- CodeChallenge/Code Challenge.js
+++ CodeChallenge/Code Challenge.js
@@ -1,18 +1,19 @@
 
-    let button_Color = document.querySelector('.random-click'); 
-        button_Color.addEventListener('click', change_Color );
+ 
 
+//Số trúng là 2,4,6,10;
 
-    function change_Color(){
-        let background = document.querySelector('.backgound');
-        let color = ["black", "red", "blue", "yellow" , "gray" ,"pink", "orange"]
-        let random = color[random_Color(color)];
-            background.style.backgroundColor = random;
-        }
-
-    function random_Color(inpuArray){
-        return Math.floor(Math.random()* inpuArray.length);
-        }
-
-    change_Color();
-    random_Color();
+function hienThi(){
+    let number_Corect = [2, 4, 6, 10]
+          for(let i=0; i <= number_Corect.length; i++){
+                if(input_Number.value == 2 || input_Number.value == 4 || input_Number.value == 6 || input_Number.value == 10){
+                    alert("Bạn đã chọn đúng Số");
+                    break;
+              }
+                if(input_Number.value != number_Corect[i]){
+                    alert("Số không đúng, Nhập lại");
+                    alert("Một trong các số trong dãy số đúng " + number_Corect[i]);
+                    break;
+              } 
+          }          
+    }
